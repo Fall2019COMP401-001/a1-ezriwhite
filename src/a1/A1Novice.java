c96645fa303282265a9ac17d01bfb5f9bcf66580
ezri <ezri@ezris-mbp.wireless-1x.unc.edu>
2019-08-29 16:08:50 -0400

    first attempt

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,32 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// total number of customers
+		
+		int totalCustomers = scan.nextInt();
+		
+		
+		
+		// reading information
+		
+		for (int i=0; i <= totalCustomers; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			char initial = firstName.charAt(0);
+			Double totalItems = scan.nextDouble();
+			Double totalPrice = (double) 0;
+				for (int j=0; j < totalItems; j++) {
+					Double amount = scan.nextDouble();
+					String item = scan.next();
+					Double cost = scan.nextDouble();
+					totalPrice += amount * cost;
+				}
+			System.out.println(initial + ". " + lastName + ": " + totalPrice);
+		}
+		
+		
+		// All input parsed, so close scanner
+				scan.close();
+		
 	}
 }
