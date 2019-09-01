275fb26b60c81814c9f79b6fc0b5ac7ba7150240
ezri <ezri@ezris-mbp.wireless-1x.unc.edu>
2019-08-31 22:19:21 -0400

    fingrs crossed

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,56 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		// total products
+		
+				int totalProducts = scan.nextInt();
+				
+				// array of products
+				
+				Product[] inventory = new Product[totalProducts];
+				
+				// putting products and properties into array
+				
+				for (int i=0; i < inventory.length; i++ ) {
+					inventory[i] = new Product(scan.next(), scan.nextDouble());
+					
+				}
+				
+				// total customers
+				
+				int totalCustomer = scan.nextInt();
+				
+				// array of customers
+				
+				Customer[] customers = new Customer[totalCustomer];
+				
+				// putting customers into array
+				
+				for (int i=0; i < customers.length; i++ ) {
+					customers[i] = new Customer(scan.next(),scan.next(), scan.nextInt());
+					for (int j=0; j < customers[i].totalProducts; j++) {
+						int number = scan.nextInt();
+						String item = scan.next();
+						for (int k=0; k < inventory.length; k++) {
+							if(item.contentEquals(inventory[k].name)) {
+								double cost = number * inventory[k].price;
+								customers[i].spent += cost;
+								inventory[k].numBought += number;
+								inventory[k].numCustomers +=1;
+							}
+						}
+					}	
+				}
+				scan.close();
+				
+				// loop to list and print
+				
+				for( int i=0; i < inventory.length; i++) {
+					if (inventory[i].numCustomers == 0) {
+						System.out.println("No customers bought " + inventory[i].name);
+					} else {
+						System.out.println(inventory[i].numCustomers + " customers bought " + inventory[i].numBought + " " + inventory[i].name);
+					}
+				}
 	}
 }
--- src/a1/Product.java
+++ src/a1/Product.java
@@ -3,6 +3,8 @@ package a1;
 public class Product {
 	String name;
 	double price;
+	int numBought = 0;
+	int numCustomers = 0;
 	
 	public Product(String n, double p) {
 		name = n;
